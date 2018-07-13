89ec55e17a7e16bee1c3ced208fff512bbef30e3
wang gang <wang.home.gang@linuxbox.com>
2018-07-13 17:21:36 +0800

    page.go add NewJSONView and NewYAMLView

--- page.go
+++ page.go
@@ -5,27 +5,32 @@ import (
 	"html/template"
 	"io"
 	"errors"
+	"encoding/json"
+	"gopkg.in/yaml.v2"
 )
 
 type (
-	View struct{
+	view struct{
 		render func(w io.Writer,data interface{})error
 	}
+	View interface{
+		Render(ctx context.Context,data interface{}) error
+	}
 )
-func NewTemplateView(tmpl *template.Template) *View{
-	return &View{render:func(w io.Writer ,data interface{}) error{
+func NewTemplateView(tmpl *template.Template) View{
+	return &view{render:func(w io.Writer ,data interface{}) error{
 		return tmpl.Execute(w,data)
 	}}
 }
-func (p View)Render(ctx context.Context, data interface{}) error{
+func (p *view)Render(ctx context.Context, data interface{}) error{
 		w,_,err := ReadHttpContext(ctx)
 		if err != nil {
 			return err
 		}
 	return p.render(w,data)
 }
-func NewStringView() *View{
-	return &View{render:func(w io.Writer, data interface{})error{
+func NewStringView() View{
+	return &view{render:func(w io.Writer, data interface{})error{
 		value,ok := data.(string)
 		if ok {
 			_,err:=w.Write([]byte(value))
@@ -37,7 +42,23 @@ func NewStringView() *View{
 		return NotStringError
 	}}
 }
-
+func NewJSONView() View{
+	return &view{
+		render:func(w io.Writer,data interface{})error{
+			en:=json.NewEncoder(w)
+			return en.Encode(data)
+		},
+	}
+}
+func NewYAMLView() View{
+	return &view{
+		render:func(w io.Writer,data interface{}) error{
+			en:=yaml.NewEncoder(w)
+			defer en.Close()
+			return en.Encode(data)
+		},
+	}
+}
 var (
 	NotStringError = errors.New("Not string")
 )
